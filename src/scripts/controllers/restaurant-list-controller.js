a19819767edec74be37b40613f516c570e158a44
afonso.praca <afonsoinfo@gmail.com>
2014-02-01 21:49:39 -0200

    responsive

--- src/scripts/controllers/restaurant-list-controller.js
+++ src/scripts/controllers/restaurant-list-controller.js
@@ -1,6 +1,6 @@
 ﻿angular.module("controllers")
 	.controller("RestaurantListController", function($scope, restaurantList, RestaurantFilterService,
-                                                   RestaurantPaginationService, SharedData){
+                                                   RestaurantPaginationService, SharedData, $window){
 
 		var self = this;
 		$scope.RestaurantFilterService = RestaurantFilterService;
@@ -15,6 +15,16 @@
 			$scope.list = restaurantList.data;
 			$scope.listToDisplay = restaurantList.data;
 			$scope.paging.totalCount = $scope.list.length;
+		};
+
+		$scope.getNuberOfCollums = function(){
+			// console.log($($window).width());
+			if ($($window).width() < 768){
+				return 2;
+			} else {
+				return 3;
+			}
+
 		};
 
 		self.startModule();
