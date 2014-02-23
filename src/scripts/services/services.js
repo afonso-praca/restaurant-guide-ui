577eb3fb2c9c4a2311db1b9e5128e525769498b0
afonso.praca <afonsoinfo@gmail.com>
2014-02-23 17:38:22 -0300

    api disqus

--- src/scripts/services/services.js
+++ src/scripts/services/services.js
@@ -41,7 +41,7 @@ angular.module("services")
 			getDisqusCounter: function(urlArray){
 				return $http({
 					method: 'GET',
-					url: self.endpoint + "https://disqus.com/api/3.0/threads/set.jsonp",
+					url: "https://disqus.com/api/3.0/threads/set.jsonp",
 					data: { api_key: "mQXEUI6cVnMEMrTu7zt5S4ISfDgTffQ8KUi54F7Gk2siiAiJm6yLjLVN4feTrxKt", forum : "penedorj", thread : urlArray },
 					cache: true
 				})
