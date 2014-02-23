e6771a75e551a5b9252b4d4dfa460e9dd5941fbe
afonso.praca <afonsoinfo@gmail.com>
2014-02-23 17:02:02 -0300

    adjusts on fade in

--- src/scripts/directives/directives.js
+++ src/scripts/directives/directives.js
@@ -4,6 +4,7 @@ angular.module("directives", [])
 			restrict: 'A',
 			link: function(scope, element) {
 				element.bind('load', function() {
+					$(element).parent().parent().parent().css("margin-top", "20px");
 					$(element).parent().fadeIn();
 				});
 			}
