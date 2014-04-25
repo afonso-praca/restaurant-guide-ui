93d4198d00db28bf9a46e1f15b1b08df412e2ae3
afonso.praca <afonsoinfo@gmail.com>
2014-04-24 23:31:48 -0300

    adds comment form and comment list on restaurant details sends comment to api

--- src/scripts/controllers/restaurant-detail-controller.js
+++ src/scripts/controllers/restaurant-detail-controller.js
@@ -1,24 +1,53 @@
 angular.module("controllers")
-	.controller("RestaurantDetailController", function($scope, restaurant, SharedData, restaurantGuideService){
+	.controller("RestaurantDetailController", function($scope, restaurant, SharedData, restaurantGuideService, $rootScope){
 
 		var self = this;
 		$scope.SharedData = SharedData;
+		$scope.restaurant = restaurant.data.restaurant;
+		$scope.similars = restaurant.data.similars;
+
+		$scope.currentComment = {
+			title: "",
+			body: "",
+			stars: 0,
+			user_id: 33,
+			restaurant_id: $scope.restaurant._id
+		};
 
 		self.startModule = function(){
-			$scope.restaurant = restaurant.data.restaurant;
-			$scope.similars = restaurant.data.similars;
+
 		};
 
 		$scope.addComent = function(){
-			alert("Essa funcionalidade estara disponivel em breve!");
-//			restaurantGuideService.addComment(
-//				{
-//					comment: "Muito Bom",
-//					stars: 3,
-//					user: "3344556677",
-//					restaurant_id: $scope.restaurant._id
-//				}
-//			);
+			//alert("Essa funcionalidade estara disponivel em breve!");
+			restaurantGuideService.addComment($scope.currentComment).then(
+				function(){
+					console.log("sucesso");
+					$scope.currentComment = {
+						title: "",
+						body: "",
+						stars: 0,
+						user_id: 33,
+						restaurant_id: $scope.restaurant._id
+					};
+					SharedData.isLoading = true;
+					restaurantGuideService.getRestaurantDetails($scope.restaurant._id).then(
+						function(restaurant){
+							$scope.restaurant = restaurant.data.restaurant;
+							$scope.similars = restaurant.data.similars;
+							SharedData.isLoading = false;
+						},
+						function(){
+							console.log("erro");
+							SharedData.isLoading = false;
+						}
+					);
+				},
+				function(){
+					console.log("erro");
+					SharedData.isLoading = false;
+				}
+			);
 		};
 
 		self.startModule();
