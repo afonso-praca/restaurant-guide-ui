4d1e68f62c34f0ec8adce6cc22a866c6a19785ec
afonso.praca <afonsoinfo@gmail.com>
2014-05-24 22:26:09 -0300

    adiciona ordem nos similares

--- src/scripts/filters/filters.js
+++ src/scripts/filters/filters.js
@@ -1,1 +1,17 @@
 angular.module("filters", []);
+
+angular.module("filters")
+	.filter('orderByRelevance', function(){
+		return function (input){
+			input.sort(function(a,b){
+				if (a.comments.length < b.comments.length)
+					return -1;
+				if (a.comments.length === b.comments.length)
+					return 0;
+				if (a.comments.length > b.comments.length)
+					return 1;
+				return 0;
+			});
+			return input;
+		};
+	});
