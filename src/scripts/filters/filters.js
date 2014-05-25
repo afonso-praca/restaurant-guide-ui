984992e84d4b329f11f6e24d5fc56a6b8ce47a14
afonso.praca <afonsoinfo@gmail.com>
2014-05-24 22:31:32 -0300

    ajusta ordem

--- src/scripts/filters/filters.js
+++ src/scripts/filters/filters.js
@@ -4,11 +4,11 @@ angular.module("filters")
 	.filter('orderByRelevance', function(){
 		return function (input){
 			input.sort(function(a,b){
-				if (a.comments.length < b.comments.length)
+				if (a.comments.length > b.comments.length)
 					return -1;
 				if (a.comments.length === b.comments.length)
 					return 0;
-				if (a.comments.length > b.comments.length)
+				if (a.comments.length < b.comments.length)
 					return 1;
 				return 0;
 			});
